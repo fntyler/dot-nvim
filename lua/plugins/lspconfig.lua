52a74ab9b4e0e1b04d2e4b1fae03590cc74784e4
fntyler <71953103+fntyler@users.noreply.github.com>
2024-07-22 23:35:46 -0400

    fix: remove file from being sourced
    mod no longer required
    
    likely to remove from tracked files in the future

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,8 @@
 -- neovim/nvim-lspconfig
-local lspconfig = require('lspconfig')
-lspconfig.pyright.setup {}
+local lsp = require('lspconfig')
+--require('lspconfig').pyright.setup({})
+--require('lspconfig').ansiblels.setup({})
+
 
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
