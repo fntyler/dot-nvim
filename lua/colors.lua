385334dea325d4748e51e3797b6ea290ab1b02e5
fntyler <71953103+fntyler@users.noreply.github.com>
2024-11-23 15:19:40 -0500

    fix: default colorscheme

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,14 +1,12 @@
 -- colorscheme
-
 if not vim.opt.termguicolors:get() then
     vim.opt.termguicolors = true
 end
 
+vim.opt.background = 'dark'
 
-if vim.opt.background == 'dark' then
-    vim.opt.background = 'dark'
+if vim.opt.background:get() == 'dark' then
     vim.cmd.colorscheme("tokyonight-night")
 else
-    vim.opt.background = 'light'
     vim.cmd.colorscheme("tokyonight-day")
 end
