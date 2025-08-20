4b71d3fb45027b0b04bae59565dbf8eb1c4e1f24
fntyler <71953103+fntyler@users.noreply.github.com>
2025-08-20 16:25:56 -0400

    feat: global hidden files
    when using `find_files` picker, include hidden files but still ignore
    `.git/`

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,13 @@
 -- -- nvim-telescope
 require("telescope").setup {
+  defaults = {
+    file_ignore_patterns = { "%.git/" }
+  },
+  pickers = {
+    find_files = {
+      hidden = true
+    }
+  },
   extensions = {
     fzf = {}
   }
