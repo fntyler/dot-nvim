1c73d84d56b114d77a62def1a55a27b8757a58bb
fntyler <71953103+fntyler@users.noreply.github.com>
2025-10-28 11:14:46 -0400

    chore(obsidian): include default picker definition

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -140,6 +140,25 @@ require("obsidian").setup({
     },
   },
 
+  picker = {
+    -- Set your preferred picker. Can be one of 'telescope.nvim', 'fzf-lua', or 'mini.pick'.
+    name = "telescope.nvim",
+    -- Optional, configure key mappings for the picker. These are the defaults.
+    -- Not all pickers support all mappings.
+    note_mappings = {
+      -- Create a new note from your query.
+      new = "<C-x>",
+      -- Insert a link to the selected note.
+      insert_link = "<C-l>",
+    },
+    tag_mappings = {
+      -- Add tag(s) to current note.
+      tag_note = "<C-x>",
+      -- Insert a tag at the current location.
+      insert_tag = "<C-l>",
+    },
+  },
+
   sort_by = "modified",
   sort_reversed = true,
 
