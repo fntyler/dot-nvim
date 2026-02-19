15b5678bdbc773b445c0cad21ac9bbadd6af1682
fntyler <71953103+fntyler@users.noreply.github.com>
2026-02-19 18:05:00 -0500

    feat(oil): hidden files default

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -117,10 +117,12 @@ return {
     'stevearc/oil.nvim',
     ---@module 'oil'
     ---@type oil.SetupOpts
-    opts = {},
     -- Optional dependencies
     dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
+    config = function()
+      require("plugins.oil")
+    end,
   },
 }
