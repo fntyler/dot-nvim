20ab3cc981f491a1f8e2b7bf78fa43ad4570f98a
fntyler <71953103+fntyler@users.noreply.github.com>
2026-02-19 12:36:43 -0500

    feat(oil): add oil.nvim

--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -112,8 +112,15 @@ return {
     end
   },
 
-  -- ai sidekick
-  --{
-  --  "folke/sidekick.nvim",
-  --},
+  -- file explorer
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 }
